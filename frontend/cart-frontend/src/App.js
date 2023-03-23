e809c083f0402a1b92f624a79e957f3ddface269
Pavi99 <pavithra@smbapps.com.au>
2023-03-23 12:16:01 +0530

    cart adding implemented

--- frontend/cart-frontend/src/App.js
+++ frontend/cart-frontend/src/App.js
@@ -5,10 +5,13 @@ import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div>
+      <ToastContainer />
       <NavBar />
       <Routes>
         <Route path="/cart" element={<Cart />} />
