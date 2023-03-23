50e448bb721a718c13fca8b62bcdbee6d500374c
Pavi99 <pavithra@smbapps.com.au>
2023-03-23 09:15:21 +0530

    fronend redux store created

--- frontend/cart-frontend/src/components/Home.jsx
+++ frontend/cart-frontend/src/components/Home.jsx
@@ -1,4 +1,8 @@
+import { useGetAllProductsQuery } from "../features/productsApi";
+
 const Home = () => {
+    const { data, error, isLoading } = useGetAllProductsQuery();
+    console.log(data, error, isLoading);
     return(
         <div>
             home
