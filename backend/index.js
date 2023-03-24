2b39c418f87954200ee6623f531618c964ba0632
Pavi99 <pavithra@smbapps.com.au>
2023-03-24 18:14:27 +0530

    authentication added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const mongooose = require("mongoose");
 
 const products = require("./services/products");
 const app = express();
+const register = require("./routes/register");
+const login = require("./routes/login");
 
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
+app.use("/api/register", register);
+app.use("/api/login", login);
 
 app.get("/", (req, res) => {
     res.send("Welcome to our online shop");
@@ -16,5 +22,14 @@ app.get("/products", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
+console.log(uri);
 
-app.listen(5000, console.log("server is running on port 5000"));
+app.listen(port, console.log(`server is running on port ${port}`));
+
+mongooose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connection successful.."))
+.catch((err) => console.log("MongoDb connection failed", err.message));
